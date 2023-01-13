a1f12619ce5c89a9c15f5522b521b6d426018bb5
alfianchii <pianaee379@gmail.com>
2023-01-13 23:08:10 +0700

    add: contact method in SiteController.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -20,9 +20,22 @@ class SiteController extends Controller
     }
 
     // [SiteController::class, "contact"]
-    public function contact()
+    public function contact(Request $request, Response $response)
     {
+        $contact = new ContactForm();
+
+        if ($request->isPost()) {
+            $contact->loadData($request->getBody());
+
+            if ($contact->validate() && $contact->send()) {
+                Application::$app->session->setFlash("success", "Thanks for contacting us!");
+                return $response->redirect("/contact");
+            }
+        }
+
         // Return layouts + views
-        return $this->render("contact");
+        return $this->render("contact", [
+            "model" => $contact
+        ]);
     }
 }
