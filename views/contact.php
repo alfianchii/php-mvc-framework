b737e36338e1d315d8240ba8302cd166c2e12af1
alfianchii <pianaee379@gmail.com>
2023-01-13 23:17:22 +0700

    fix: contact.php

--- views/contact.php
+++ views/contact.php
@@ -1,17 +1,21 @@
+<?php
+
+/** @var $this \app\core\View */
+/** @var $this \app\models\ContactForm */
+
+use app\core\form\TextareaField;
+
+$this->title = "Contact";
+
+?>
+
 <h1>Contact us</h1>
 
-<form method="POST" action="">
-    <div class="form-group">
-        <label>Subject</label>
-        <input type="text" name="subject" class="form-control">
-    </div>
-    <div class="form-group">
-        <label>Email</label>
-        <input type="text" name="email" class="form-control">
-    </div>
-    <div class="form-group">
-        <label>Body</label>
-        <textarea name="body" class="form-control"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php $form = \app\core\form\Form::begin("", "POST") ?>
+
+<?= $form->field($model, "subject") ?>
+<?= $form->field($model, "email") ?>
+<?= new TextareaField($model, "body") ?>
+<button type="submit" class="btn btn-primary">Submit</button>
+
+<?php \app\core\form\Form::end(); ?>
