7d739d6e450e193e75a56fdb29f4ca07e4dfd3f1
alfianchii <pianaee379@gmail.com>
2023-01-13 23:04:20 +0700

    add: show title in main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -15,7 +15,8 @@ use app\core\Application;
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <!-- The $this will be corresponds to the View.php -->
+    <title><?= $this->title ?></title>
 </head>
 
 <body>
