0fbde3ab2d08e38d95b5a2ee65c79d2873ee0868
alfianchii <pianaee379@gmail.com>
2023-01-13 22:57:42 +0700

    delete: remove all of views in Router.php

--- core/Router.php
+++ core/Router.php
@@ -50,7 +50,7 @@ class Router
 
         // If string, render Views
         if (is_string($callback)) {
-            return $this->renderView($callback);
+            return Application::$app->view->renderView($callback);
         }
 
         // If array, instance it [controller, method]
@@ -76,53 +76,5 @@ class Router
         // such as handleContact() in SiteController.php.
         // And also the $this->response just like the $this->request
         return call_user_func($callback, $this->request, $this->response);
-    }
-
-    // VIEWS
-    // Render layouts + views
-    public function renderView($view, $params = [])
-    {
-        $layoutContent = $this->layoutContent();
-        $viewContent = $this->renderOnlyView($view, $params);
-
-        // Replace the {{content}} inside $layoutContent, with $viewContent
-        return str_replace("{{content}}", $viewContent, $layoutContent);
-    }
-
-    // Render the content (not file)
-    public function renderContent($viewContent)
-    {
-        $layoutContent = $this->layoutContent();
-
-        // Replace the {{content}} inside $layoutContent, with $viewContent
-        return str_replace("{{content}}", $viewContent, $layoutContent);
-    }
-
-    // Render layouts
-    protected function layoutContent()
-    {
-        // By default, the layout will be "main"
-        $layout = Application::$app->layout;
-
-        // But if the controller was exists, take the layout from the controller
-        if (Application::$app->controller) {
-            $layout = Application::$app->controller->layout;
-        }
-
-        ob_start();
-        include_once Application::$ROOT_DIR . "/views/layouts/$layout.php";
-        return ob_get_clean();
-    }
-
-    // Render views
-    protected function renderOnlyView($view, $params)
-    {
-        foreach ($params as $key => $value) {
-            $$key = $value;
-        }
-
-        ob_start();
-        include_once Application::$ROOT_DIR . "/views/$view.php";
-        return ob_get_clean();
     }
 }
