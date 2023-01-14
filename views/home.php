b24b649540b906da3e571b36ee01e84435f3603c
alfianchii <pianaee379@gmail.com>
2023-01-14 15:01:19 +0700

    add: title of home.php

--- views/home.php
+++ views/home.php
@@ -1,2 +1,19 @@
+<?php
+
+// Annotating
+/** @var $name \app\controllers\SiteController */
+
+use app\core\Application;
+
+// Set the title of home page
+$this->title = "Home";
+
+?>
+
 <h1>Home</h1>
-<h3>Welcome <?= $name ?></h3>
+
+<?php if (Application::isGuest()) : ?>
+<h3>Welcome!</h3>
+<?php else : ?>
+<h3>Welcome, <?= $name ?>!</h3>
+<?php endif; ?>
