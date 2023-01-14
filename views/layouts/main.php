73182a647aae832fe516084015731125143e17e4
alfianchii <pianaee379@gmail.com>
2023-01-14 15:59:20 +0700

    fix: toast

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -87,6 +87,13 @@ use app\core\Application;
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
         integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
     </script>
+
+    <script>
+    // Toast
+    const myToast = document.getElementById("myToast");
+    const toast = new bootstrap.Toast(myToast);
+    toast.show();
+    </script>
 </body>
 
 </html>
