039005ca8112a76003fae725ec5572720d0fbf00
alfianchii <pianaee379@gmail.com>
2023-01-14 14:48:26 +0700

    fix: m0002_add_password_column.php commentings

--- migrations/m0002_add_password_column.php
+++ migrations/m0002_add_password_column.php
@@ -4,12 +4,14 @@ use app\core\Application;
 
 class m0002_add_password_column
 {
+    // Create the password field
     public function up()
     {
         $db = Application::$app->db;
         $db->pdo->exec("ALTER TABLE users ADD COLUMN password VARCHAR(512) NOT NULL");
     }
 
+    // Drop the password field
     public function down()
     {
         $db = Application::$app->db;
