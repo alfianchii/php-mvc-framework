4e814e65bc0f27a248d9198f176422c0e91fb565
alfianchii <pianaee379@gmail.com>
2023-01-14 16:43:04 +0700

    style: section wraps all of elements of register.php

--- views/register.php
+++ views/register.php
@@ -8,22 +8,24 @@ $this->title = "Register";
 
 ?>
 
-<h1>Create an account</h1>
+<section class="mt-5">
+    <h1>Create an account</h1>
 
-<?php $form = \app\core\form\Form::begin("", "POST"); ?>
+    <?php $form = \app\core\form\Form::begin("", "POST"); ?>
 
-<div class="row">
-    <div class="col">
-        <?= $form->field($model, "firstname") ?>
+    <div class="row">
+        <div class="col">
+            <?= $form->field($model, "firstname") ?>
+        </div>
+        <div class="col">
+            <?= $form->field($model, "lastname") ?>
+        </div>
     </div>
-    <div class="col">
-        <?= $form->field($model, "lastname") ?>
-    </div>
-</div>
 
-<?= $form->field($model, "email") ?>
-<?= $form->field($model, "password")->passwordField() ?>
-<?= $form->field($model, "confirmPassword")->passwordField() ?>
-<button type="submit" class="btn btn-primary">Submit</button>
+    <?= $form->field($model, "email") ?>
+    <?= $form->field($model, "password")->passwordField() ?>
+    <?= $form->field($model, "confirmPassword")->passwordField() ?>
+    <button type="submit" class="btn btn-primary">Submit</button>
 
-<?php \app\core\form\Form::end(); ?>
+    <?php \app\core\form\Form::end(); ?>
+</section>
