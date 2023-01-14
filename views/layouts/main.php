77ede629a86d5cb24fa180a0b2c24519104905d6
alfianchii <pianaee379@gmail.com>
2023-01-14 15:58:54 +0700

    fix: alert to toast when registration

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -63,10 +63,19 @@ use app\core\Application;
 
     <div class="container">
         <?php if (Application::$app->session->getFlash("success")) : ?>
-        <div class="alert alert-success">
-            <?= Application::$app->session->getFlash("success") ?>
+        <div class="position-absolute bottom-0 end-0 mb-3 me-3">
+            <div id="myToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
+                <div class="toast-header bg-primary text-white">
+                    <strong class="me-auto">Notification</strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div class="toast-body">
+                    <?= Application::$app->session->getFlash("success") ?>
+                </div>
+            </div>
         </div>
         <?php endif; ?>
+
         {{content}}
     </div>
 
@@ -74,6 +83,7 @@ use app\core\Application;
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
         integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous">
     </script>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
         integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
     </script>
