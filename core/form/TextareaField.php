f88edacf689255c5915384ef9f88fde517c2d46f
alfianchii <pianaee379@gmail.com>
2023-01-14 16:19:56 +0700

    style: add id in textarea field

--- core/form/TextareaField.php
+++ core/form/TextareaField.php
@@ -8,7 +8,8 @@ class TextareaField extends BaseField
     public function renderInput(): string
     {
         return sprintf(
-            '<textarea name="%s" class="form-control %s">%s</textarea>',
+            '<textarea name="%s" id="%s" class="form-control %s">%s</textarea>',
+            $this->attribute,
             $this->attribute,
             $this->model->hasError($this->attribute) ? "is-invalid" : "",
             $this->model->{$this->attribute},
